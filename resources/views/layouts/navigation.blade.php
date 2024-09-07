6d29b125332c192bc1cfe05faccadd9eb3120dbc
Robin Joshua R. Hermocilla <robinjoshuar.hermocilla@Robins-MacBook-Air.local>
2024-09-07 16:57:33 +0800

    commented the permissions

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,29 +17,29 @@
                     </x-nav-link>
                 </div>
 
-                @can('view permissions')
+                {{-- @can('view permissions') --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('permissions.index')" :active="request()->routeIs('permissions.index')">
                         {{ __('Permissions') }}
                     </x-nav-link>
                 </div>
-                @endcan
+                {{-- @endcan --}}
 
-                @can('view roles')
+                {{-- @can('view roles') --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
                         {{ __('Roles') }}
                     </x-nav-link>
                 </div>
-                @endcan
+                {{-- @endcan --}}
 
-                @can('view users')
+                {{-- @can('view users') --}}
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('Users') }}
                     </x-nav-link>
                 </div>
-                @endcan
+                {{-- @endcan --}}
 
                 @can('view tournaments')
                 @can('edit tournaments')

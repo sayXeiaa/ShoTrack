eeb30264a0ae947079d76ba3a7c85261d2c0bc4f
Robin Joshua R. Hermocilla <robinjoshuar.hermocilla@Robins-MacBook-Air.local>
2024-09-22 23:33:02 +0800

    created model for playByPlay

---
+++ app/Models/PlayByPlay.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlayByPlay extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'player_id',
+        'schedule_id',
+        'quarter',
+        'game_time',
+        'type_of_stat',
+        'result',
+        'team_A_score',
+        'team_B_score',
+    ];
+
+    public function player()
+    {
+        return $this->belongsTo(Players::class, 'player_id');
+    }
+
+}

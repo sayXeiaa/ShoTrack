868bd6ec86c546c63eb92b34a6d17580b93a45e1
Robin Joshua R. Hermocilla <robinjoshuar.hermocilla@Robins-MacBook-Air.local>
2024-09-24 23:59:39 +0800

    added guards for editing and deleting teams

--- resources/views/teams/list.blade.php
+++ resources/views/teams/list.blade.php
@@ -4,7 +4,9 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Teams') }}
             </h2>
+            @can('edit teams')
             <a href="{{ route('teams.create') }}" class="bg-slate-700 text-sm rounded-md text-white px-3 py-2">Create</a>
+            @endcan
         </div>
     </x-slot>
 
@@ -50,8 +52,10 @@
                         <th class="px-6 py-3 text-left">Sports Director</th>
                         <th class="px-6 py-3 text-left">Years in BUCAL</th>
                         <th class="px-6 py-3 text-left">Address</th>
+                        @can('edit teams')
                         <th class="px-6 py-3 text-left" width="180">Created</th>
                         <th class="px-6 py-3 text-center" width="180">Action</th>
+                        @endcan
                     </tr>
                 </thead>
                 <tbody class="bg-white">
@@ -86,7 +90,7 @@
                         <td class="px-6 py-3 text-left">
                             {{ $team->address }}
                         </td>
-                        
+                        @can ('edit teams')
                         <td class="px-6 py-3 text-left">
                             {{ \Carbon\Carbon::parse($team->created_at)->format('d M, Y') }}
                         </td>
@@ -94,6 +98,7 @@
                             <a href="{{ route('teams.edit', $team->id) }}" class="bg-slate-700 text-sm rounded-md text-white px-3 py-2 hover:bg-slate-600">Edit</a>
                             <a href="javascript:void(0);" onclick="deleteteam({{ $team->id }})" class="bg-red-600 text-sm rounded-md text-white px-3 py-2 hover:bg-red-500">Delete</a>
                         </td>
+                        @endcan
                     </tr>    
                     @endforeach
                     @endif

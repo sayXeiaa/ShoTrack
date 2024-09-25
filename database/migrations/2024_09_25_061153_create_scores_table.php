7b705543f44e636abc7508ef7de2a6b39316c1cb
Robin Joshua R. Hermocilla <robinjoshuar.hermocilla@Robins-MacBook-Air.local>
2024-09-25 14:49:26 +0800

    created migration for scores

---
+++ database/migrations/2024_09_25_061153_create_scores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('schedule_id');
+            $table->unsignedBigInteger('team_id');
+            $table->integer('quarter');
+            $table->integer('score');
+            $table->timestamps();
+
+            $table->foreign('schedule_id')->references('id')->on('schedules')->onDelete('cascade');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scores');
+    }
+};

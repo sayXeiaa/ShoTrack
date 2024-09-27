b9342d7d136403148e49c856fd29360adc0fb8d5
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2024-09-27 22:46:52 +0800

    added the field for view box score and route

--- resources/views/schedules/list.blade.php
+++ resources/views/schedules/list.blade.php
@@ -87,8 +87,14 @@ function getInitials($teamName) {
                             </div>
                         </div>
 
+                        <div class="mt-4 flex justify-center ml-[32rem]">
+                            <a href="{{ route('playerstats.index', ['schedule_id' => $schedule->id, 'team1_id' => $schedule->team1_id, 'team2_id' => $schedule->team2_id]) }}" class="text-blue-500 hover:underline">
+                                View Box Score
+                            </a>
+                        </div>
+
                         @can('edit schedule')
-                            <div class="flex space-x-2 mt-4">
+                            <div class="flex space-x-2 -mt-8">
                                 <a href="{{ route('schedules.edit', $schedule->id) }}" class="bg-slate-700 text-base rounded-md text-white px-3 py-2 hover:bg-slate-600">Edit</a>
                                 <a href="javascript:void(0);" onclick="deleteschedule({{ $schedule->id }})" class="bg-red-600 text-base rounded-md text-white px-3 py-2 hover:bg-red-500">Delete</a>
                                 <a href="{{ route('playerstats.create', ['schedule_id' => $schedule->id]) }}" class="bg-blue-600 text-base rounded-md text-white px-3 py-2 hover:bg-blue-500">Manage Game</a>

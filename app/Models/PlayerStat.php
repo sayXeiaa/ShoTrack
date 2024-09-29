8646d6ad8e64149699873aa798d7916be40fa7c2
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2024-09-29 18:38:54 +0800

    added fillable properties

--- app/Models/PlayerStat.php
+++ app/Models/PlayerStat.php
@@ -13,6 +13,32 @@ class PlayerStat extends Model
     use HasFactory;
 
     protected $table = 'player_stats';
+    protected $fillable = [
+        'player_id',
+        'schedule_id',
+        'team_id', // Ensure this column exists in your migration
+        'points',
+        'two_pt_fg_attempt',
+        'two_pt_fg_made',
+        'two_pt_percentage',
+        'three_pt_fg_attempt',
+        'three_pt_fg_made',
+        'three_pt_percentage',
+        'free_throw_attempt',
+        'free_throw_made',
+        'free_throw_percentage',
+        'free_throw_attempt_rate',
+        'rebounds',
+        'offensive_rebounds',
+        'defensive_rebounds',
+        'assists',
+        'blocks',
+        'steals',
+        'turnovers',
+        'personal_fouls',
+        'effective_field_goal_percentage',
+        'minutes',
+    ];
 
     public function player()
     {

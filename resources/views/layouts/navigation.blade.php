7c9e2ec3e49bd440ee1957537436e3aec48af0bb
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2024-09-29 21:27:31 +0800

    replaced the icon with the app name

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,10 +5,11 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-white" />
+                    <a href="{{ route('dashboard') }}" class="text-white text-lg font-bold hover:text-gray-500">
+                        ShoTrack
                     </a>
                 </div>
+                
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">

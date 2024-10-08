8b7e0c065ee3d3546f9125cc09c605429752516f
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2024-10-09 00:10:37 +0800

    added hovering effect on logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,12 @@
-<img src="{{ asset('images/shottracklogo.png') }}" alt="Shot Track Logo"  class="w-[100px]"/>
+<img src="{{ asset('images/shottracklogo.png') }}" alt="Shot Track Logo" class="w-[100px] hover-image" />
+
+<style>
+    .hover-image {
+        transition: transform 0.3s ease, opacity 0.3s ease;
+    }
+
+    .hover-image:hover {
+        transform: scale(1.1); 
+        opacity: 0.8; 
+    }
+</style>

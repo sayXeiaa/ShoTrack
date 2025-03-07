8d95a034a440c459f97121ecfbac1fae229384fc
Robin Joshua R. Hermocilla <124029762+sayXeiaa@users.noreply.github.com>
2025-03-07 09:50:41 +0800

    changed the route for back button

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -4,7 +4,7 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Roles / Edit
             </h2>
-            <a href="{{ route('permissions.index') }}" class="bg-slate-700 text-sm rounded-md text-white px-3 py-2">Back</a>
+            <a href="{{ route('roles.index') }}" class="bg-slate-700 text-sm rounded-md text-white px-3 py-2">Back</a>
         </div>
     </x-slot>
 
